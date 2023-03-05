0df12ba39181738eec540f5b5f67c7e12c49bea7
Jasmine <dr.jingyuezhao@outlook.com>
2023-03-05 11:50:21 +0000

    creage images folder and update Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,59 @@
 import React from "react";
+import Hero from "../Hero";
+
+const styles = {
+  jumbotron: {
+    position: "relative",
+    width: "100%",
+    paddingTop: 150,
+    paddingBottom: 80,
+    backgroundImage: url("../images/coding-wallpaper-3.jpg"),
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  jumboText: {
+    position: "relative",
+    color: "white",
+    fontWeight: "bold",
+    fontStyle: "italic",
+    textShadow: "4px 4px 8px white",
+  },
+  btnXl: {
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: "15px 45px",
+    borderRadius: 300,
+    marginBottom: 50,
+  }
+};
 
 function Home() {
   return (
     <div>
+      <div className="jumbotron" style={styles.jumbotron}>
+        <div className="container h-100">
+          <div className="row h-100">
+            <div className="col-lg-5 my-auto text-center">
+              <div className="mx-auto">
+                <h1 className="jumboText mb-5" style={styles.jumboText}>Welcome to my site!</h1>
+                <h2 className="jumboText mb-5" style={styles.jumboText}>My name is Jingyue.</h2>
+                <h3 className="jumboText mb-5" style={styles.jumboText}>I'm a front-end web developer.</h3>
+                <a href="/#contact" className="btn btn-outline-warning btn-xl js-scroll-trigger" style={styles.btnXl}
+                  role="button">Let's collaborate!</a>
+              </div>
+            </div>
+            <div className="col-lg-7 my-auto">
+              <div className="pic-container">
+                <img src="../../images/my-wd-img.jpg" className="img-fluid" alt="my picture" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <h1>Home Page</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
@@ -14,7 +65,7 @@ function Home() {
         consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
         malesuada fames ac ante ipsum primis in faucibus.
       </p>
-    </div>
+    </div >
   );
 }
 
