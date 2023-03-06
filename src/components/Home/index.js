45224d791b095a1f3f6312b6626bed6989187211
Jasmine <dr.jingyuezhao@outlook.com>
2023-03-06 12:36:14 +0000

    update projects.json with 6 selected projects

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,7 +12,7 @@ function Home() {
           <div className="row h-100">
             <div className="col-lg-5 my-auto text-center">
               <div className="mx-auto">
-                <h1 className="jumboText mb-5">Welcome to my site!</h1>
+                <h1 className="jumboText mb-5">Welcome to my portfolio site!</h1>
                 <h2 className="jumboText mb-5">My name is Jingyue.</h2>
                 <h3 className="jumboText mb-5">I'm a front-end web developer with a passion for creating excellent user experiences.</h3>
                 <Link to="contact" role="button" className="btn btn-warning btn-xl rounded-pill js-scroll-trigger">
