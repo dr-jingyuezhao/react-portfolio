57cb68cfa902044212d702b36ed755928ebb0563
Jasmine <dr.jingyuezhao@outlook.com>
2023-03-06 16:45:26 +0000

    rename the image for each project, update projects.json file and Projects, ProjectCard component

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,26 +3,27 @@ import { Link } from 'react-router-dom';
 import "./style.css";
 
 function ProjectCard(props) {
+  const image = require(`../../assets/images/${props.image}`)
   return (
     <div className="cardContainer col-sm-12 col-md-6 col-lg-4 mb-3">
       <div className="card">
-        <img src={props.image} className="card-img-top" alt={props.title} />
-        <div className="card-body">
-          <h5 className="card-title"><strong>Project title:</strong> {props.title}</h5>
+        <img src={image} className="card-img-top" alt={props.title} />
+        <div className="card-body text-center">
+          <h5 className="card-title text-capitalize"><strong style={{ color: "rgb(9, 155, 180)", fontSize: "30px", lineHeight: "3rem" }}>Project title: </strong><br />{props.title}</h5>
           <p className="card-text">{props.description}.</p>
-          <ul className="cardInfo">
+          <ul className="cardInfo" style={{color: "rgb(9, 155, 180)"}}>
             <li>
-              <Link className="cardInfo" to={props.appLink} style={{ color: "rgb(9, 155, 180)", fontStyle: "bold", fontSize: "20px" }}>
-              <strong>App Link</strong>
+              <Link className="cardInfo font-weight-bold" to={props.appLink} style={{ color: "rgb(9, 155, 180)", fontSize: "20px" }}>
+                <strong>The App Link</strong>
               </Link>
             </li>
             <li>
-            <Link className="cardInfo" to={props.githubRepoLink} style={{ color: "rgb(9, 155, 180)", fontStyle: "bold", fontSize: "20px" }}>
-              <strong>GitHub Repo Link</strong>
+              <Link className="cardInfo font-weight-bold" to={props.githubRepoLink} style={{ color: "rgb(9, 155, 180)", fontSize: "20px" }}>
+                <strong>GitHub Repo Link</strong>
               </Link>
             </li>
           </ul>
-          <a href="https://github.com/dr-jingyuezhao" className="btn btn-primary">Learn More</a>
+          <a className="moreBtn btn btn-info" href="https://github.com/dr-jingyuezhao" target="_blank" rel="noopener noreferrer" role="button">Learn More</a>
         </div>
       </div>
     </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -16,7 +16,7 @@ function Projects(props) {
                     <div className="container-fluid">
                       <div className="row projects">
                         {/* Project card: Project title, Link to the deployed version, Link to the GitHub repository, GIF or screenshot of the deployed application */}
-                        {projects.map(project => <ProjectCard title={project.title} description={project.description} appLink={project.appLink} githubRepoLink={project.githubRepoLink} imageURL={project.image} />)}
+                        {projects.map(project => <ProjectCard title={project.title} description={project.description} appLink={project.appLink} githubRepoLink={project.githubRepoLink} image={project.image} />)}
                       </div>
                     </div>
                   </section>
